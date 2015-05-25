e26f0f9c7a70209a4fdd3a27bdcca394ad825b11
Romain Prignon <pro.rprignon@gmail.com>
2015-05-25 11:43:44 +0000

    ajout de deux method pour l'ajout et recuperation du score

--- peaks/firebase.js
+++ peaks/firebase.js
@@ -1,1 +1,17 @@
 var flappyPeaksFirebaseRef = new Firebase("https://flappypeaks.firebaseio.com/highscores");
+var date = new Date().toLocaleString();
+
+function saveHighscore (email, bestScore) {
+    var childRef = flappyPeaksFirebaseRef.push();
+    childRef.set({
+        email: email,
+        bestscore: bestScore,
+        date: date
+    });
+}
+
+function findBestHighScore() {
+    flappyPeaksFirebaseRef.orderByChild("bestscore").limitToLast(1).once("value", function(data) {
+        console.log(data.val());
+    });
+}
